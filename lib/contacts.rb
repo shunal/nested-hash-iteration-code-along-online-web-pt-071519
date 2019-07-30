56fb7c431a38cff6d2ce6d61a505e614bc2dff2f
Steven Hunal <stevehunal@gmail.com>
2019-07-30 18:52:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  
-end
+  if person == "Freddy Mercury"
+    contact_details_hash do |attribute, data|
+    end
+  end
 end
